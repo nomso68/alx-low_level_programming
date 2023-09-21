c72c8ca2d8cdac01ca7a07016ce789b8aa06d6e5
nomso68 <oganchinomso2014@gmail.com>
2023-09-21 16:10:27 -0700

    a

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,16 +12,15 @@ char *_strncat(char *dest, char *src, int n)
 	int i;
 	int j;
 
-
 	i = 0;
 	while (dest[i] != '\0')
 	{
 		i++;
 	}
 	j = 0;
-	while (j < n && src[i] != '\0')
+	while (j < n && src[j] != '\0')
 	{
-	dest[i] = src[i];
+	dest[i] = src[j];
 	i++;
 	j++;
 	}
