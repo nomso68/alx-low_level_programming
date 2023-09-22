cfda2d675139666352996e3152d5edd6588daa1f
nomso68 <oganchinomso2014@gmail.com>
2023-09-22 15:59:38 -0700

    a

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,9 +12,9 @@ char *cap_string(char *str)
 	while (str[index])
 	{
 		while (!(str[index] >= 'a' && str[index] <= 'z'))
-			index ++;
+			index++;
 
-		if(str[index - 1] == ' ' ||
+		if (str[index - 1] == ' ' ||
 			str[index - 1] == '\t' ||
 			str[index - 1] == '\n' ||
 			str[index - 1] == ',' ||
